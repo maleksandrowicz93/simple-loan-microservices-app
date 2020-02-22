98b617ddacab710c970cab551950f7c3ba44f601
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-02-22 05:19:46 +0100

    add Column annotations, id field and constructor

--- springboot-customer-api/src/main/java/com/github/maleksandrowicz93/springbootcustomerapi/Customer.java
+++ springboot-customer-api/src/main/java/com/github/maleksandrowicz93/springbootcustomerapi/Customer.java
@@ -4,15 +4,27 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter @NoArgsConstructor
 public class Customer {
 
-    private Integer creditId;
-    private String firstName;
-    private String pesel;
-    private String surname;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    private Integer creditId;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String surname;
+    @Column(nullable = false, unique = true, length = 13)
+    private String pesel;
+
+    public Customer(String firstName, String surname, String pesel) {
+        this.firstName = firstName;
+        this.surname = surname;
+        this.pesel = pesel;
+    }
 }
