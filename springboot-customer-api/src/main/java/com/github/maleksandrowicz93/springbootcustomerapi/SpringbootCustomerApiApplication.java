465763e723491d85fc089eaf5b5e02515cfccc8f
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-02-22 04:44:15 +0100

    add customer-api component and configure it

---
+++ springboot-customer-api/src/main/java/com/github/maleksandrowicz93/springbootcustomerapi/SpringbootCustomerApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.maleksandrowicz93.springbootcustomerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCustomerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCustomerApiApplication.class, args);
+	}
+
+}
