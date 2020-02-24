8b158a1771f334b548ebfb205979aa62edc3362a
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-02-24 04:23:35 +0100

    add credit-api component and configure it

---
+++ springboot-credit-api/src/main/java/com/github/maleksandrowicz93/springbootcreditapi/AppConfig.java
@@ -0,0 +1,9 @@
+package com.github.maleksandrowicz93.springbootcreditapi;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+}
